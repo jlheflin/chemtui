af2967d42d59553b62dd9403f1737fa401f84126
Jacob Heflin <91171563+jlheflin@users.noreply.github.com>
2026-06-22 10:23:48 -0500

    Full reset

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
-#include <spdlog/spdlog.h>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/app.hpp>
+#include <ftxui/dom/elements.hpp>
 
 int main(int argc, char* argv[]) {
-  spdlog::info("Hello world!");
+  auto screen = ftxui::Screen::Create(
+    ftxui::Dimension::Full(),
+    ftxui::Dimension::Fixed(10)
+  );
+
+  auto& cell = screen.CellAt(10, 5);
+
+  cell.character = "X";
+  cell.foreground_color = ftxui::Color::Red;
+  cell.background_color = ftxui::Color::RGB(0, 255, 0);
+  cell.bold = true;
+
+  screen.Print();
+
+  return 0;
 }
